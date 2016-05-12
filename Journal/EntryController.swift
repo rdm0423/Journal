69f138e6345b63c667277666f54f4938da172c90
Ross McIlwaine <rdm0423@gmail.com>
2016-05-12 13:29:50 -0600

    Adds Dictionary & NSUserDefaults
    App now saves, and returns items users add

--- Journal/EntryController.swift
+++ Journal/EntryController.swift
@@ -10,24 +10,43 @@ import Foundation
 
 class EntryController {
     
+    private let kEntries = "entries"
+    
     static let sharedController = EntryController()
     
     var entries: [Entry]
     
     init() {
         self.entries = []
+        loadFromPersistentStorage()
     }
     
     func addEntry(entry: Entry) {
         
         entries.append(entry)
+        saveToPersistentStorage()
     }
     
     func removeEntry(entry: Entry) {
         
         if let entryIndex = entries.indexOf(entry) {
-        
+            
             entries.removeAtIndex(entryIndex)
+            saveToPersistentStorage()
         }
+        
+    }
+    func saveToPersistentStorage() {
+        
+        NSUserDefaults.standardUserDefaults().setObject(entries.map{$0.dictionaryCopy()}, forKey: kEntries)
+    }
+    
+    func loadFromPersistentStorage() {
+        
+        guard let entriesDictionaryArray = NSUserDefaults.standardUserDefaults().objectForKey(kEntries) as? [[String:AnyObject]] else {
+            
+            return
+        }
+        entries = entriesDictionaryArray.flatMap{Entry(dictionary: $0)}
     }
 }
